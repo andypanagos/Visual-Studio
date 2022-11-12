03488a3fdadc18668e821ef9d1279bc1e1bb8cf4
andypanagos <andypanagos2@gmail.com>
2022-11-12 14:34:58 -0500

    Visual Studio Assignment

---
+++ VS HTML Assignment/VS HTML Assignment/wwwroot/js/JavaScript1.js
@@ -0,0 +1,10 @@
+﻿function myFunction() {
+    let text;
+    let person = prompt("Who Will Win the World Cup?:", "");
+    if (person == null || person == "") {
+        text = "";
+    } else {
+        text = " LETS GO!! " + person + " will win the World Cup!!";
+    }
+    document.getElementById("demo").innerHTML = text;
+}
